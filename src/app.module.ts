51021b583e0dec303d5dc0ba3a6861a1f89ae602
kkpagaev <kkpagaev@gmail.com>
2023-12-17 06:28:01 +0200

    Revert "remove user module"
    This reverts commit e93e65b39281a853974e823286d463b3788c1c3a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DrizzleModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DrizzleModule,
+  ],
   providers: [
-    UsersService,
-    UsersController,
     {
       provide: APP_PIPE,
       useClass: ZodValidationPipe,
